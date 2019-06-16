8c924e55eccc9c07619490506e599d29f3e22183
Shizishengsheng <49809551+Shizishengsheng@users.noreply.github.com>
2019-06-16 19:13:20 +0800

    Create menu.h

---
+++ src/Menu/menu.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "cocos2d.h"					
+using namespace cocos2d;
+
+class GameMenu : public Layer
+
+{
+public:    static Scene* createScene();
+
+		   virtual bool init();
+
+		   CREATE_FUNC(GameMenu);
+
+		   void EnterHitBrick(Ref *pSender);
+
+		   void EnterHelp(Ref *pSender);
+
+};
+
